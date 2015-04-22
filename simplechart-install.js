7e06863bdb2797816d4a555bc05c490e61fde434
Josh Kadis <josh@alleyinteractive.com>
2015-04-22 16:14:49 -0700

    updated script but the clone promise doesn't fire now :(

--- simplechart-install.js
+++ simplechart-install.js
@@ -1,59 +1,78 @@
+// Node dependencies
 var Git = require('nodegit');
 var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf');
 
-var mediaExplorerPath = '../media-explorer';
-var simpleChartPath = 'app';
+// repos needed
+mediaExplorerRepo = 'https://github.com/Automattic/media-explorer.git';
+simplechartRepo = 'https://github.com/alleyinteractive/simplechart.git';
+
+// setup vars
+var GITHUB_TOKEN;
+var mediaExplorerPath = __dirname + '/../media-explorer';
+var simplechartPath = __dirname + '/app';
+var simplechartTmp = __dirname + '/_tmp_simplechart';
 
 /**
- * do a fresh intall of local simplechart app no matter what
+ * required for Github HTTPS public repo cloning
+ */
+function githubHttpsCloneOptions() {
+  var  cloneOptions = {};
+  cloneOptions.remoteCallbacks = {
+    certificateCheck: function() {
+      return 1;
+    }
+  };
+  return cloneOptions;
+}
+
+/**
+ * pull down a fresh install of Simplechart web app no matter what
  */
 function setupLocalSimplechart() {
-	// required for Github HTTPS repo cloning
-	var GITHUB_TOKEN = '22b250143d3a6d1d20d81aa448362ee7225476ef';
-	var	cloneOptions = {};
-	cloneOptions.remoteCallbacks = {
-		certificateCheck: function() { return 1; },
-		credentials: function(url, userName) {
-			return Git.Cred.userpassPlaintextNew(GITHUB_TOKEN, 'x-oauth-basic');
-		}
-	};
+  var  cloneOptions = githubHttpsCloneOptions();
+  // used API token for private repo cloning
+  cloneOptions.remoteCallbacks.credentials = function(url, userName) {
+    return Git.Cred.userpassPlaintextNew(GITHUB_TOKEN, 'x-oauth-basic');
+  }
 
-	var tmpLocation = __dirname + '/_tmp_simplechart';
-	console.log('Installing Simplechart web app in ' + tmpLocation );
-	Git.Clone('https://github.com/alleyinteractive/simplechart.git', tmpLocation, cloneOptions )
-		.then( function(repo){
-			console.log( 'Moving web app to ' + __dirname + '/app' );
-			fs.renameSync( tmpLocation + '/client/pages', __dirname + '/app' );
-			rimraf( tmpLocation, function(err){
-				if (err) throw err;
-				process.exit(1);
-			});
-		});
+  // delete existing stuff
+  rimraf.sync(simplechartTmp);
+  rimraf.sync(simplechartPath);
+
+  console.log('Downloading Simplechart repo to ' + simplechartTmp);
+  Git.Clone(simplechartRepo, simplechartTmp, cloneOptions).then(function(repo) {
+    // move the standalone web app then delete the temp folder
+    console.log('Moving web app to ' + simplechartApp);
+    fs.renameSync(simplechartTmp + '/client/pages', simplechartApp);
+    console.log('Deleting ' + simplechartTmp);
+    rimraf.sync(simplechartTmp);
+    process.exit(1);
+  });
 }
 
 /**
  * install Media Explorer plugin if not already installed
  */
 function installMediaExplorer(err, files) {
-	// required for Github HTTPS public repo cloning
-	var	cloneOptions = {};
-	cloneOptions.remoteCallbacks = {
-		certificateCheck: function() { return 1; }
-	};
-
-	if ( typeof files === 'undefined' || files.length === 0 ) {
-		console.log( 'Installing Media Exlorer' );
-		Git.Clone('https://github.com/Automattic/media-explorer.git', __dirname + '/' + mediaExplorerPath, cloneOptions )
-			.then( function(repo){
-				setupLocalSimplechart();
-			});
-	} else {
-		console.log( 'Media Explorer already installed' );
-		setupLocalSimplechart();
-	}
+  if (typeof files === 'undefined' || files.length === 0) {
+    console.log('Installing Media Exlorer plugin');
+    var  cloneOptions = githubHttpsCloneOptions();
+    Git.Clone(mediaExplorerRepo, mediaExplorerPath, cloneOptions)
+      .then(function(repo) {
+        setupLocalSimplechart();
+      });
+  }
+  else {
+    console.log('Media Explorer plugin already installed');
+    setupLocalSimplechart();
+  }
 }
 
-// install Media Explorer
-fs.readdir( mediaExplorerPath, installMediaExplorer );
+// start by trying to get Github API token from github_token.txt
+fs.readFile('github_token.txt', {encoding: 'utf8'}, function(err, data) {
+  if (err) throw err;
+  GITHUB_TOKEN = data;
+  fs.readdir(mediaExplorerPath, installMediaExplorer);
+});
