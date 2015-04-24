08436fbcf2c720966d7c6f362ac72fe6c5a11374
Josh Kadis <josh@alleyinteractive.com>
2015-04-24 14:08:53 -0700

    setup script working now

--- simplechart-install.js
+++ simplechart-install.js
@@ -1,3 +1,8 @@
+/**
+ * Installs the Simplechart WordPress plugin
+ * After the script runs, this folder will NOT be a Git repo
+ */
+
 // Node dependencies
 var Git = require('nodegit');
 var fs = require('fs');
@@ -10,9 +15,42 @@ simplechartRepo = 'https://github.com/alleyinteractive/simplechart.git';
 
 // setup vars
 var GITHUB_TOKEN;
-var mediaExplorerPath = __dirname + '/../media-explorer';
+var mediaExplorerPath;
 var simplechartPath = __dirname + '/app';
 var simplechartTmp = __dirname + '/_tmp_simplechart';
+
+
+/**
+ * get path to the plugins directory where we want to install Media Explorer
+ */
+ function getMediaExplorerPath() {
+  var pluginsDir = [];
+
+  // if we are in the plugins/ directory already
+  if ('plugins' === __dirname.split('/').reverse()[1]) {
+    pluginsDir = __dirname.split('/');
+    pluginsDir.pop();
+  }
+  // if we are somewhere else, i.e. a theme
+  else {
+    var wpContentFound = false;
+    __dirname.split('/').forEach(function(value, key){
+      // move upwards through directories until we get to wp-content/
+      if (!wpContentFound) {
+        pluginsDir.push(value);
+      }
+      if ('wp-content' == value) {
+        wpContentFound = true;
+      }
+    });
+    // then add plugins/
+    pluginsDir.push('plugins');
+  }
+
+  // pluginsDir should now be an array of directories leading up to wp-content/plugins/
+  pluginsDir.push('media-explorer');
+  return pluginsDir.join('/');
+ }
 
 /**
  * required for Github HTTPS public repo cloning
@@ -41,16 +79,16 @@ function setupLocalSimplechart() {
   rimraf.sync(simplechartTmp);
   rimraf.sync(simplechartPath);
 
-  console.log('Downloading Simplechart repo to ' + simplechartTmp);
-  Git.Clone.clone(simplechartRepo, simplechartTmp, cloneOptions)
-  	.then(function(repo) {
-	  // move the standalone web app then delete the temp folder
-	  console.log('Moving web app to ' + simplechartApp);
-	  fs.renameSync(simplechartTmp + '/client/pages', simplechartApp);
-	  console.log('Deleting ' + simplechartTmp);
-	  rimraf.sync(simplechartTmp);
-	  process.exit(1);
-	});
+  console.log('Downloading Simplechart repo');
+  Git.Clone.clone(simplechartRepo, simplechartTmp, cloneOptions).done(function(){
+    // move the standalone web app then delete the temp folder
+    console.log('Moving web app');
+    fs.renameSync(simplechartTmp + '/client/pages', simplechartPath);
+    console.log('Deleting temp folder');
+    rimraf.sync(simplechartTmp);
+    console.log('Setup complete!');
+    process.exit(1);
+  });
 }
 
 /**
@@ -60,14 +98,10 @@ function installMediaExplorer(err, files) {
   if (typeof files === 'undefined' || files.length === 0) {
     console.log('Installing Media Exlorer plugin');
     var  cloneOptions = githubHttpsCloneOptions();
-    Git.Clone.clone(mediaExplorerRepo, mediaExplorerPath, cloneOptions)
-      .then(function(repo) {
-        setupLocalSimplechart();
-      });
+    Git.Clone.clone(mediaExplorerRepo, mediaExplorerPath, cloneOptions);
   }
   else {
     console.log('Media Explorer plugin already installed');
-    setupLocalSimplechart();
   }
 }
 
@@ -75,5 +109,7 @@ function installMediaExplorer(err, files) {
 fs.readFile('github_token.txt', {encoding: 'utf8'}, function(err, data) {
   if (err) throw err;
   GITHUB_TOKEN = data;
+  mediaExplorerPath = getMediaExplorerPath();
   fs.readdir(mediaExplorerPath, installMediaExplorer);
+  setupLocalSimplechart();
 });
