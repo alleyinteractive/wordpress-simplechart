a212f466caaf3d4fefede362cb1060e0a5902240
Josh Kadis <josh@alleyinteractive.com>
2016-07-14 15:21:54 -0400

    render new chart in page template, asynchronously loading widget JS once per page

--- modules/class-simplechart-template.php
+++ modules/class-simplechart-template.php
@@ -10,7 +10,6 @@ class Simplechart_Template {
 		add_shortcode( 'simplechart', array( $this, 'render_shortcode' ) );
 		add_action( 'wp', array( $this, 'add_filter_post_content') );
 		add_action( 'wp_enqueue_scripts', array( $this, 'frontend_enqueues' ) );
-		add_action( 'wp_head', array( $this, 'print_app_host' ) );
 	}
 
 	public function frontend_enqueues(){
@@ -39,26 +38,32 @@ class Simplechart_Template {
 	// render the chart from template
 	public function render( $id ){
 
-		// only allow non-published charts if we're looking at a post preview
+		// only allow non-published charts unless we're looking at a post preview
 		if ( 'publish' !== get_post_status( $id ) && ! is_preview() ){
 			return '';
 		}
 
-		$loader_url = Simplechart::instance()->get_config( 'widget_loader_url' );
+		if ( 'simplechart' !== get_post_type( intval( $id ) ) ) {
+			return '';
+		}
+		?>
+			<div
+				id="simplechart-widget-<?php echo esc_attr( $id ); ?>"
+				class="simplechart-widget"
+				data-url="<?php echo esc_url( home_url( '/simplechart/api/' . $id . '/' ) ); ?>"
+			></div>
+			<script>
+				<?php // Load Simplechart widget JS asynchronously if not already loaded ?>
+				if ( ! document.getElementById( 'simplechart-widget-js' ) ) {
+					var scriptEl = document.createElement( 'script' ),
+						chartEl = document.getElementById( 'simplechart-widget-<?php echo esc_attr( $id ); ?>' );
 
-		// support versions that predate loading by chart ID
-		$json_data = get_post_meta( $id, 'simplechart-data', true );
-		$image_fallback = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'large' );
-		$template_format = file_get_contents( Simplechart::instance()->get_plugin_dir() . 'templates/template-partial.html' );
-
-		$template_html = sprintf( $template_format,
-			json_encode( json_decode( $json_data ), JSON_HEX_APOS ),
-			Simplechart::instance()->save->validate_template_fragment( $template_html ),
-			esc_url( $loader_url ),
-			( ! $image_fallback ? '' : esc_url( $image_fallback[0] ) )
-		);
-
-		return $template_html;
+					scriptEl.id = 'simplechart-widget-js';
+					scriptEl.src = <?php echo wp_json_encode( Simplechart::instance()->get_config( 'widget_loader_url' ) ); ?>;
+					chartEl.parentNode.appendChild( scriptEl );
+				}
+			</script>
+		<?php
 	}
 
 	// automatically render chart if looking at the chart's own post
@@ -76,14 +81,4 @@ class Simplechart_Template {
 
 		return $template_html . $content;
 	}
-
-	// print app host as JS var in head if overriding simplechart.io
-	public function print_app_host(){
-		if ( is_admin() ){
-			return;
-		}
-		// set app host URL
-		echo "\n<script> window.simplechartWidgetDirUrl = window.simplechartWidgetDirUrl || " . json_encode( Simplechart::instance()->get_config( 'widget_dir_url' ) ) . "; </script>\n";
-	}
-
 }
