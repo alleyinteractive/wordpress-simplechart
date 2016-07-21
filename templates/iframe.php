9e34c11a3a810336aafa2191e7f54491cbabebf8
Josh Kadis <josh@alleyinteractive.com>
2016-07-21 13:52:44 -0400

    phpcs tab indentation

--- templates/iframe.php
+++ templates/iframe.php
@@ -4,26 +4,26 @@
 
 <!DOCTYPE html>
 <html>
-  <head lang="en">
-    <meta charset="UTF-8">
-    <title>Simplechart</title>
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont,
-          "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
-          "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-      }
-      html, body, #app {
-        height: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <div id='app'></div>
-    <script src="<?php echo esc_url( Simplechart::instance()->get_config( 'web_app_js_url' ) ); ?>"></script>
-  </body>
+	<head lang="en">
+	<meta charset="UTF-8">
+	<title>Simplechart</title>
+		<style>
+			body {
+			margin: 0;
+			padding: 0;
+			font-family: -apple-system, BlinkMacSystemFont,
+				"Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell",
+				"Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+			}
+			html, body, #app {
+				height: 100%;
+			}
+		</style>
+	</head>
+	<body>
+	<div id='app'></div>
+	<script src="<?php echo esc_url( Simplechart::instance()->get_config( 'web_app_js_url' ) ); ?>"></script>
+	</body>
 </html>
 
 <?php die(); ?>
