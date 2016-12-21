9cd25246ae3929071ee3b71459c75de6798e34c5
Davis Shaver <davis@alleyinteractive.com>
2016-12-21 14:54:30 -0500

    Don't change props if element doesn't exist

--- js/plugin/plugin.js
+++ js/plugin/plugin.js
@@ -1,8 +1,10 @@
 jQuery( document ).ready(function() {
-	jQuery(
-		'input[value="wordpress-simplechart/simplechart.php"]'
-	)[0].disabled = true;
-	jQuery(
-		'input[value="wordpress-simplechart/simplechart.php"]'
-	).css('cursor', 'default');
+	if (jQuery('input[value="wordpress-simplechart/simplechart.php"]').length) {
+		jQuery(
+			'input[value="wordpress-simplechart/simplechart.php"]'
+		)[0].disabled = true;
+		jQuery(
+			'input[value="wordpress-simplechart/simplechart.php"]'
+		).css('cursor', 'default');
+	}
 });
