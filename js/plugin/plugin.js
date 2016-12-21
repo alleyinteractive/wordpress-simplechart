95c9796ff2f4154b3612860082252c4f9b28dc7b
Davis Shaver <davis@alleyinteractive.com>
2016-12-21 13:51:15 -0500

    Change cursor to default when disabled

--- js/plugin/plugin.js
+++ js/plugin/plugin.js
@@ -2,4 +2,7 @@ jQuery( document ).ready(function() {
 	jQuery(
 		'input[value="wordpress-simplechart/simplechart.php"]'
 	)[0].disabled = true;
+	jQuery(
+		'input[value="wordpress-simplechart/simplechart.php"]'
+	).css('cursor', 'default');
 });
