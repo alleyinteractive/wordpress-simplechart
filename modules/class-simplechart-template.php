47d3bd57e5934a804be3f24f3338b473ee888ad7
Max Eddy <max@alleyinteractive.com>
2017-06-13 11:17:39 -0400

    Fix amp placeholder with a default value that will let the iframe load, even if it's near the top of the screen.

--- modules/class-simplechart-template.php
+++ modules/class-simplechart-template.php
@@ -96,6 +96,7 @@ class Simplechart_Template {
 			add_action( 'simplechart_iframe_footer', array( $this, 'iframe_scripts' ) );
 			require( $instance->get_plugin_dir( 'templates/embed.php' ) );
 		} else if ( ! $disable_amp ) {
+			add_filter( 'simplechart_amp_iframe_placeholder', array( $this, 'default_placeholder'), 10, 2);
 			require( $instance->get_plugin_dir( 'templates/amp-iframe.php' ) );
 		}
 	}
@@ -149,7 +150,16 @@ class Simplechart_Template {
 		return $template_html . $content;
 	}
 
-	public function iframe_scripts( ) {
+	public function iframe_scripts() {
 		do_action( 'wp_print_footer_scripts' );
+	}
+
+	public function default_placeholder( $id, $content ) {
+		if (!$content) {
+			$src = esc_url( Simplechart::instance()->get_plugin_url('/images/placeholder.png') );
+			return '<amp-img layout="fixed" width="311" height="61" src="' . $src . '" placeholder></amp-img>';
+		}
+
+		return $content;
 	}
 }
